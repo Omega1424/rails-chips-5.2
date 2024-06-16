547c1baa233041b13bd9b7c47b53a807e737c4d0
Austin Isaac Isaac <1007099@mymail.sutd.edu.sg>
2024-06-16 06:41:24 +0000

    part 1 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,12 +1,5 @@
-class Movie < ActiveRecord::Base
+class Movie < ApplicationRecord
   def self.with_ratings(ratings_list)
-    # if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all
-    #  movies with those ratings
-    # if ratings_list is nil, retrieve ALL movies
-    if ratings_list.blank?
-      all  # If no ratings are selected, return all movies
-    else
-      where(rating: ratings_list)  # Filter movies to those with the specified ratings
-    end
+    where(rating: ratings_list)
   end
 end
